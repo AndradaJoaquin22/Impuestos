bd459d04f4536c4a97781348161fcc60a14f03d6
andradajoaquin22 <andradajoaquin22@gmail.com>
2023-07-02 20:20:58 -0300

    p

---
+++ Impuestos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impuestos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float sueldo;
+            string lista;
+            Console.Write("Ingrese su sueldo: ");
+            lista = Console.ReadLine();
+            sueldo= float.Parse(lista);
+            if (sueldo > 3000) 
+            {
+                Console.Write("Esta persona paga impuestos");
+            }
+            else { Console.Write("Esta persona no paga impuestos"); }
+            Console.ReadKey();
+        }
+    }
+}
